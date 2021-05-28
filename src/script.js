e075018344fce876a48313547f68685bbdfc5433
Hércules Lopes <69056516+herculeslopes@users.noreply.github.com>
2021-05-28 17:46:26 -0300

    2 update

--- src/script.js
+++ src/script.js
@@ -1,7 +1,24 @@
 let lblQuestion = document.getElementById('lbl-question')
 let inputLine = document.getElementById('entry-line')
 
+document.addEventListener("keydown", recordKey)
+
 let gameOn = false
+let points = 0
+
+function recordKey(e) {
+    let value = Number(e.key)
+        if (Number.isInteger(value) && inputLine !== document.activeElement) {
+            inputLine.value += e.key
+        } else if (e.key == "Enter") {
+            enter()
+        }    
+}
+
+function reset() {
+    lblQuestion.innerText = `GAME OVER\nTotal de pontos: ${points}`
+    points = 0
+}
 
 function perguntar() {
     gameOn = true
@@ -12,12 +29,17 @@ function perguntar() {
     res = n1 * n2
 }
 
+function clearInput() {
+    inputLine.value = ""
+}
+
 function typeNum(num) {
     inputLine.value += num
 }
 
 function enter(){
     let value = inputLine.value
+    inputLine.value = ""
 
     lblQuestion.style.color = "#46435F"
 
@@ -25,14 +47,16 @@ function enter(){
 
         if (value == res) {
             // let lblColor = lblQuestion.style.color
+            points++
             lblQuestion.style.color = "green"
-            lblQuestion.textContent = "ACERTOU!"
+            lblQuestion.innerText = `ACERTOU!\nSeus pontos: ${points}`
             // lblQuestion.style.color = lblColor
             gameOn = false
         } else {
             lblQuestion.style.color = "red"
-            lblQuestion.textContent = "ERROU!"
+            lblQuestion.innerText = `ERROU!\nResposta correta: ${res}`
             gameOn = false
+            reset()
         }
 
     } else {
